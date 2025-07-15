5312e8cc7685516ace7a391c6d4690c591ee2fb6
moniwa-r <r.moniwa@gmail.com>
2025-07-15 15:54:23 +0900

    Update human page Navbar logo link and iframe spacing

--- human/src/App.js
+++ human/src/App.js
@@ -10,7 +10,7 @@ function App() {
       {/* Fixed Header */}
       <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
         <Container>
-          <Navbar.Brand href="#home">Asolab. 3DGS</Navbar.Brand>
+          <Navbar.Brand href="https://serene-genie-62b2bc.netlify.app/">Asolab. 3DGS</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
